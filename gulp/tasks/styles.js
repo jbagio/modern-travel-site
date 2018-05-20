b278e31f85e760c45d9ef841edf5fa1a3b3d4ecd
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-05-20 21:01:37 +0100

    Add media queries mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,11 @@ const autoprefixer = require('autoprefixer');
 const cssVars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const cssImport = require('postcss-import');
+const mixins = require('postcss-mixins');
 
 gulp.task('styles', () => {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
     .on('error', function (err) {
       console.log(err.toString());
       this.emit('end');
