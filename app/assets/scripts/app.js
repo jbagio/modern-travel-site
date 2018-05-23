b78a1b9dc9e57786ffdabf95ea0f30c63e7a3148
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-05-23 01:15:41 +0100

    Finish mobile header menu

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,18 +1,3 @@
-//////// TESTING STUFF
-//////// TO BE REMOVED IN A LATER LECTURE
+import MobileMenu from './modules/MobileMenu';
 
-//const Person = require('./modules/Person');
-import Person from './modules/Person';
-
-const person1 = new Person('Mistah Frodo');
-person1.greet();
-
-class TaxPayer extends Person {
-  payTaxes () {
-    console.log(`${this.name} is now a slave to the system.`);
-  }
-}
-
-const person2 = new TaxPayer('Uncle Baggins');
-person2.greet();
-person2.payTaxes();
+const mobileMenu = new MobileMenu();
