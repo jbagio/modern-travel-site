a4d62040bf24a152a2956e5f9d52049a9fd606fc
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-05-23 18:07:44 +0100

    Finish sticky header styling

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
+import $smoothScroll from 'jquery-smooth-scroll';
 import waypoint from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class StickyHeader {
   constructor () {
     this.siteHeader = $('.site-header');
     this.headerTrigger = $('.large-hero__title');
+    this.headerLinks = $('.primary-nav a');
+    this.pageSections = $('.page-section');
     this.createHeaderWaypoint();
+    this.createPageSectionWaypoints();
+    this.addSmoothScroll();
+  }
+
+  addSmoothScroll () {
+    this.headerLinks.smoothScroll();
   }
 
   createHeaderWaypoint () {
@@ -17,8 +26,38 @@ class StickyHeader {
           $(self.siteHeader).addClass('site-header--dark');
         } else {
           $(self.siteHeader).removeClass('site-header--dark');
+          self.headerLinks.removeClass("is-current-link");
         }
       },
+    });
+  }
+
+  createPageSectionWaypoints () {
+    let self = this;
+    this.pageSections.each((index, elem) => {
+      new Waypoint({
+        element: elem,
+        handler: (direction) => {
+          if (direction == 'down') {
+            let matchingLink = elem.getAttribute('data-matching-link');
+            self.headerLinks.removeClass('is-current-link');
+            $(matchingLink).addClass('is-current-link');
+          }
+        },
+        offset: '18%'
+      });
+
+      new Waypoint({
+        element: elem,
+        handler: (direction) => {
+          if (direction == 'up') {
+            let matchingLink = elem.getAttribute('data-matching-link');
+            self.headerLinks.removeClass('is-current-link');
+            $(matchingLink).addClass('is-current-link');
+          }
+        },
+        offset: '-35%'
+      });
     });
   }
 }
