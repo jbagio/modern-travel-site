e1070a4cb144ca1d81b16392e9c992c6d6950461
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-05-23 10:19:03 +0100

    Finish feature items and testimonials reveal on scroll

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+import waypoint from '../../../../node_modules/waypoints/lib/noframework.waypoints'
+
+class RevealOnScroll {
+  constructor (elems, offset) {
+    this.itemsToReveal = elems;
+    this.offset = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially () {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  createWaypoints () {
+    let self = this;
+    this.itemsToReveal.each((index, elem) => {
+      new Waypoint({
+        element: elem,
+        handler: () => {
+          $(elem).addClass('reveal-item--is-visible');
+        },
+        offset: self.offset
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
